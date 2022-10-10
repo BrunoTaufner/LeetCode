e9b159230190bcf500981ac32057a5b2b89c2bd1
BrunoTaufner <brunotaufner08@gmail.com>
2022-10-10 10:34:30 -0300

    Problems Solution

--- Python/1328.py
+++ Python/1328.py
@@ -3,13 +3,26 @@ class Solution(object):
     def breakPalindrome(palindrome: str) -> str:
         if len(palindrome) == 1:
             return ''
-        word = ''
+        index_changed = 0
+        num_changing = 30
         i = 0
         while i < len(palindrome):
-            printx(ord(palindrome[i]))
+            num = ord(palindrome[i]) - 96
+
+            if len(palindrome) % 2 == 0 and num - 1 <= num_changing:
+                if num == 2:
+                    palindrome[i] = chr(num - 1)
+                    break
+                elif num > 2:
+                    num_changing = num - 1
+                    index_changed = i
+
+
+            else:
+                k = 0
             i += 1
 
         return palindrome
 
 
-print(Solution().breakPalindrome('aabbcc'))
+print(Solution().breakPalindrome('az'))
