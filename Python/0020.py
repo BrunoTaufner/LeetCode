dd4a739eeaebe9f910d46c45721d91ccfa970d3c
BrunoTaufner <brunotaufner08@gmail.com>
2022-10-23 16:46:21 -0300

    Problems Solution

---
+++ Python/0020.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pilha = []
+        if len(s) % 2 != 0:
+            return False
+        for c in s:
+            try:
+                if c == '(' or c == '[' or c == '{':
+                    pilha.append(c)
+                else:
+                    abre_c = pilha.pop()
+                    if abre_c == '(':
+                        fecha_c = ')'
+                    elif abre_c == '[':
+                        fecha_c = ']'
+                    else:
+                        fecha_c = '}'
+                    if fecha_c != c:
+                        return False
+            except:
+                return False
+        if len(pilha) > 0:
+            return False
+        return True
+
+
+print(Solution().isValid('(('))
